503fb3d06f7249d032188a98e8e1f66620302b0a
Jose Manuel Aranda Gutiérrez <79646195+Arzenin@users.noreply.github.com>
2023-11-11 11:47:24 +0100

    Update DDSI_S1.java Implementada función Borrado e Inserción
    Se ha implementado la Primera versión del borrado y la inserción de tablas

--- src/src/DDSI_S1.java
+++ src/src/DDSI_S1.java
@@ -5,6 +5,44 @@ import java.sql.Statement;
 import java.sql.SQLException;
 
 public class DDSI_S1 {
+    public void Crear_Borrar_Insertar(Connection connection){
+        Statement statement = null;
+        ResultSet resultSet = null;
+        sentencia=conexion.createStatement();
+        String sql_sentencia = "DROP TABLE DETALLE_PEDIDO;"+"DROP TABLE DETALLE_PEDIDO;"+"DROP TABLE STOCK;";
+        sentencia.executeUpdate(sql_sentencia);
+        sql_sentencia = "CREATE TABLE Stock("
+            +"Cproducto int,"
+            +"Cantidad int,"
+            +"primary key(Cproducto));";
+        sentencia.executeUpdate(sql_sentencia);
+        sql_sentencia = "CREATE TABLE Pedido("
+            +"Cpedido int,"
+            +"Ccliente int,"
+            +"Fecha_pedido DATE,"
+            +"primary key(Cpedido));";
+        sentencia.executeUpdate(sql_sentencia);
+        sql_sentencia = "CREATE TABLE Detalle_Pedido("
+            +"Cpedido int,"
+            +"Cproducto int,"
+            +"Cantidad int,"
+            +"PRIMARY KEY(Cpedido,Cproducto),"
+            +"FOREIGN KEY(Cproducto) REFERENCES Stock(Cproducto),"
+            +"FOREIGN KEY(Cpedido) REFERENCES Pedido(Cpedido));";
+        sentencia.executeUpdate(sql_sentencia);
+        sql_sentencia="INSERT INTO Stock(Cproducto,Cantidad) VALUES(1,50)"+
+            "INSERT INTO Stock(Cproducto,Cantidad) VALUES(2,50)"+
+            "INSERT INTO Stock(Cproducto,Cantidad) VALUES(3,50)"+
+            "INSERT INTO Stock(Cproducto,Cantidad) VALUES(4,50)"+
+            "INSERT INTO Stock(Cproducto,Cantidad) VALUES(5,50)"+
+            "INSERT INTO Stock(Cproducto,Cantidad) VALUES(6,50)"+
+            "INSERT INTO Stock(Cproducto,Cantidad) VALUES(7,50)"+
+            "INSERT INTO Stock(Cproducto,Cantidad) VALUES(8,50)"+
+            "INSERT INTO Stock(Cproducto,Cantidad) VALUES(9,50)"+
+            "INSERT INTO Stock(Cproducto,Cantidad) VALUES(10,50)";
+        sentencia.executeUpdate(sql_sentencia);
+        connection.commit();
+    }
     public static void main(String[] args) {
         String jdbcUrl = "jdbc:oracle:thin:@oracle0.ugr.es:1521/practbd.oracle0.ugr.es"; // Reemplaza con la URL de tu base de datos Oracle
         String username = ""; // Reemplaza con tu nombre de usuario
