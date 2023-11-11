10ed89b2b06dacd0804d86332784a96c4f8a45fd
gsanzguerrero <146436092+gsanzguerrero@users.noreply.github.com>
2023-11-11 15:33:53 +0100

    Update DDSI_S1.java Crear pedido
    Lo he dejado bonito y he hehco crear pedido

--- src/src/DDSI_S1.java
+++ src/src/DDSI_S1.java
@@ -5,6 +5,9 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
 public class DDSI_S1 {
     
     String jdbcUrl = "jdbc:oracle:thin:@oracle0.ugr.es:1521/practbd.oracle0.ugr.es"; // Reemplaza con la URL de tu base de datos Oracle
@@ -48,25 +51,48 @@ public class DDSI_S1 {
         }
         
     }
-    public void Crear_Borrar_Insertar(){
-        try{
-            String sql_sentencia = "DROP TABLE DETALLE_PEDIDO";
+    public void Crear_Borrar_Insertar()throws SQLException{
+        
+        try {
+            this.borrarTablas();
+        } catch (SQLException ex) {
+            System.out.println("Las tablas no existian de antes");
+        }
+            
+        this.crearTablas();
+            
+        this.crearTuplasStock();
+            
+        connection.commit();
+    }
+         
+ 
+
+    private void borrarTablas() throws SQLException {
+        String sql_sentencia = "DROP TABLE DETALLE_PEDIDO";
             sentencia.execute(sql_sentencia);
             sql_sentencia ="DROP TABLE PEDIDO";
             sentencia.execute(sql_sentencia);
             sql_sentencia = "DROP TABLE STOCK";
             sentencia.execute(sql_sentencia);
-            sql_sentencia = "CREATE TABLE Stock("
+
+            connection.commit();
+    }
+    
+    private void crearTablas() throws SQLException {
+        String sql_sentencia =sql_sentencia = "CREATE TABLE Stock("
                 +"Cproducto int,"
                 +"Cantidad int,"
                 +"primary key(Cproducto))";
             sentencia.executeUpdate(sql_sentencia);
+            
             sql_sentencia = "CREATE TABLE Pedido("
                 +"Cpedido int,"
                 +"Ccliente int,"
                 +"Fecha_pedido DATE,"
                 +"primary key(Cpedido))";
             sentencia.executeUpdate(sql_sentencia);
+            
             sql_sentencia = "CREATE TABLE Detalle_Pedido("
                 +"Cpedido int,"
                 +"Cproducto int,"
@@ -75,7 +101,11 @@ public class DDSI_S1 {
                 +"FOREIGN KEY(Cproducto) REFERENCES Stock(Cproducto),"
                 +"FOREIGN KEY(Cpedido) REFERENCES Pedido(Cpedido))";
             sentencia.executeUpdate(sql_sentencia);
-            sql_sentencia="INSERT INTO Stock(Cproducto,Cantidad) VALUES(1,50)";
+    }
+    
+    private void crearTuplasStock() throws SQLException{
+        
+        String sql_sentencia="INSERT INTO Stock(Cproducto,Cantidad) VALUES(1,50)";
             sentencia.executeUpdate(sql_sentencia);
             sql_sentencia= "INSERT INTO Stock(Cproducto,Cantidad) VALUES(2,50)";
             sentencia.executeUpdate(sql_sentencia);
@@ -95,17 +125,45 @@ public class DDSI_S1 {
             sentencia.executeUpdate(sql_sentencia);
             sql_sentencia="INSERT INTO Stock(Cproducto,Cantidad) VALUES(10,50)";
             sentencia.executeUpdate(sql_sentencia);
-            connection.commit();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        } 
     }
-    public static void main(String[] args) {
-        DDSI_S1 bd = new DDSI_S1("x6520115","x6520115");
-        bd.Iniciar_Sesion();
-        bd.Crear_Borrar_Insertar();
-        bd.Cerrar_Sesion();
+    
+    public void hacerPedido() throws SQLException {
+        System.out.println("VA USTED A HACER UN PEDIDO: ");
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Por favor introduzca el código de pedido: ");
+        String cPedido=scanner.next();
+        System.out.println("Por favor introduzca el código de cliente: ");
+        String cCliente=scanner.next();
+        System.out.println("Por favor introduzca la fecha del pedido: ");
+        String fechaPedido=scanner.next();
+        
+        String hacerPedido = "INSERT INTO PEDIDO VALUES('"
+                + cPedido + "', '"
+                + cCliente + "', '"
+                + fechaPedido + "')";
+        sentencia.executeUpdate(hacerPedido);
         
     }
+    
+    
+    public static void main(String[] args) {
+        DDSI_S1 bd = new DDSI_S1("x7147456","x7147456");
+        bd.Iniciar_Sesion();
+        try {
+            bd.Crear_Borrar_Insertar();
+            System.out.println("Tablas Creadas");
+        }catch(Exception e){
+            System.out.println("Las tablas ya estaban creadas");     
+            //System.exit(0);
+        }
+        try {
+            bd.hacerPedido();
+            System.out.println("Pedido hecho");
+        }catch(Exception e){
+            System.out.println("Error en el pedido.");     
+        }
+        bd.Cerrar_Sesion();
+    }
+   
 }
